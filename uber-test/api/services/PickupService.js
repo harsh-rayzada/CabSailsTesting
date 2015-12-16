1050ad1ba6f84ed788062e06f1f7c60db652d9c9
harsh-rayzada <harsh.rayzada@gmail.com>
2015-12-16 15:24:35 +0530

    bug testing

--- uber-test/api/services/PickupService.js
+++ uber-test/api/services/PickupService.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 exports.pickupLogin = function(code, callback){
 	request
-    .post('https://login.uber.com/oauth/v2/token?client_secret=CS0D2nvlx4_BRUry7GlrEjdP-Sh7sD7HNVP-Gr2g&client_id=XJyYPJd9aKwtIHRmnx_gEksS0VLQ1WHc&grant_type=authorization_code&redirect_uri=http://localhost:2244/user/pickupLogin&code='+code, function(err, httpResp, body){
+    .post('https://login.uber.com/oauth/v2/token?client_secret=CS0D2nvlx4_BRUry7GlrEjdP-Sh7sD7HNVP-Gr2g&client_id=XJyYPJd9aKwtIHRmnx_gEksS0VLQ1WHc&grant_type=authorization_code&redirect_uri=http://52.74.30.237:2244/user/pickupLogin&code='+code, function(err, httpResp, body){
       if(err){
       	callback(err, null);
       }else{
